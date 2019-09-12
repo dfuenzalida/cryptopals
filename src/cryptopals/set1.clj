65c32d05ec67d632d33e7366c4c8c61cb6cf4d57
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2019-09-11 23:54:32 -0700

    Set 1, challenge 2

--- src/cryptopals/set1.clj
+++ src/cryptopals/set1.clj
@@ -1,16 +1,30 @@
 (ns cryptopals.set1)
 
+(defn- read-bytes [s]
+  (->> s
+       (partition 2)
+       (map #(apply str %))
+       (map #(Integer/parseInt % 16))))
+
+;; Challenge 1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn hex->base64
   "Reads an string of hexadecimal digits, returns a base64 representation"
   [s]
   (let [encoder (java.util.Base64/getEncoder)]
-    (->> s
-         (partition 2)
-         (map #(apply str %))
-         (map #(Integer/parseInt % 16))
+    (->> (read-bytes s)
          byte-array
          (.encode encoder)
          (map char)
          (apply str))))
-
   
+;; Challenge 2
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn fixed-xor [s1 s2]
+  (let [bytes1 (read-bytes s1)
+        bytes2 (read-bytes s2)]
+    (->> (map bit-xor bytes1 bytes2)
+         (map #(format "%02x" %))
+         (apply str))))
--- test/cryptopals/set1_test.clj
+++ test/cryptopals/set1_test.clj
@@ -2,7 +2,15 @@
   (:require [clojure.test :refer :all]
             [cryptopals.set1 :refer :all]))
 
+
 (deftest hex->base64-tests
-  (testing "example tests"
+  (testing "Challenge 1"
     (is (= (hex->base64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
-           "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"))))
+           "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")))
+
+  (testing "Challenge 2"
+    (is (= (fixed-xor "1c0111001f010100061a024b53535009181c"
+                      "686974207468652062756c6c277320657965")
+           "746865206b696420646f6e277420706c6179")))
+
+  )
