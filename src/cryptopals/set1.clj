a5c6ec7af3e3d79d3672d30d7e9d72c6d1f3f3f5
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2019-09-14 14:20:44 -0700

    Set 1 Challenge 6; refactor; md5 test util fn

--- src/cryptopals/set1.clj
+++ src/cryptopals/set1.clj
@@ -36,23 +36,25 @@
 
 (def hidden "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 
-(defn score
-  [message mask]
+(defn bytes-score
+  [bytes mask]
   (let [freq-phrase (apply str (reverse "ETAOIN SHRDLU"))
         upper-freqs (map vector freq-phrase (rest (range)))
         lower-freqs (map vector (.toLowerCase freq-phrase) (rest (range)))
         score-map   (into {} (concat upper-freqs lower-freqs))]
-    (->> (read-bytes message)
+    (->> bytes
          (map (comp score-map char (partial bit-xor mask)))
          (remove nil?)
          (reduce +))))
+
+(defn score [message mask]
+  (bytes-score (read-bytes message) mask))
 
 (defn decrypt-single-xor []
   (let [mask (apply max-key (partial score hidden) (range 256))]
     (->> (read-bytes hidden)
          (map (comp char (partial bit-xor mask)))
          (apply str))))
-
 
 ;; Challenge 4
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -84,12 +86,60 @@
 ;; Challenge 6
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn hamming-distance [text1 text2]
-  (let [bytes1 (map int text1)
-        bytes2 (map int text2)
-        xors   (map bit-xor bytes1 bytes2)
+(defn hamming-distance-bytes [bytes1 bytes2]
+  (let [xors   (map bit-xor bytes1 bytes2)
         bits1  (fn [acc n]
                  (if (zero? n) acc (recur (+ acc (rem n 2)) (quot n 2))))]
     (->> (map (partial bits1 0) xors)
          (reduce +))))
+
+(defn hamming-distance [text1 text2]
+  (let [bytes1 (map int text1)
+        bytes2 (map int text2)]
+    (hamming-distance-bytes bytes1 bytes2)))
+
+(defn read-input6 []
+  (let [decoder (java.util.Base64/getDecoder)]
+    (->> (slurp "resources/set1-ch6.txt")
+         (clojure.string/split-lines)
+         (clojure.string/join)
+         (.decode decoder)
+         (map int))))
+
+;; Take nblox block of keysizes ranging 2 to 40, compute the hamming distance
+;; between pairs and compute the weighted sum. Then find the lowest weight in
+;; pairs of [keysize weighted-distance]
+
+(defn find-keysize [input]
+  (let [nblox 20 ;; take 20 samples to find the actual best keysize
+        pairs (for [keysize (range 2 40)
+                    :let [blocks (take nblox (partition keysize input))
+                          dist   (map (partial apply hamming-distance-bytes)
+                                      (partition 2 1 blocks))
+                          wdist  (/ (reduce + dist) (* nblox keysize))]]
+                [keysize wdist])]
+    (->> (sort-by second pairs) ffirst)))
+
+(defn best-byte-mask [bytes]
+  (apply max-key (partial bytes-score bytes) (range 256)))
+
+(defn transpose [input keysize] ;; Returns input as blocks of keysize elems
+  (let [nblocks (quot (count input) keysize)
+        blocks  (mapv vec (partition keysize input))]
+    (for [i (range keysize)] ;; we want keysize blocks
+      (mapv #(nth % i) blocks))))
+
+(defn find-repeating-key-xor []
+  (let [input   (read-input6)
+        keysize (find-keysize input)
+        blocks  (transpose input keysize)]
+    (map best-byte-mask blocks)))
+
+(defn break-repeating-key-xor []
+  (let [xor-key    (find-repeating-key-xor)
+        key-seq    (cycle (map int xor-key))
+        text-bytes (read-input6)]
+    (->> (map bit-xor key-seq text-bytes)
+         (map char)
+         (reduce str))))
 
--- test/cryptopals/set1_test.clj
+++ test/cryptopals/set1_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [cryptopals.set1 :refer :all]))
 
+(defn- md5 [bytes]
+  (let [digester (java.security.MessageDigest/getInstance "MD5")]
+    (->> (.digest digester bytes)
+         (map (partial format "%02x"))
+         (reduce str))))
+
 (deftest hex->base64-tests
   (testing "Challenge 1"
     (is (= (hex->base64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
@@ -13,28 +19,31 @@
            "746865206b696420646f6e277420706c6179")))
 
   (testing "Challenge 3"
-    (is (= "Q29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==" ;; Base64 to hide it
-           (->> (decrypt-single-xor)
-                .getBytes
-                (.encode (java.util.Base64/getEncoder))
-                (map char)
-                (apply str)))))
+    (is (= "2aa4ac426ec0624441a0bee9b6abd80e"
+           (->> (decrypt-single-xor) .getBytes md5))))
 
   (testing "Challenge 4"
-    (is (= "4e6f77207468617420746865207061727479206973206a756d70696e670a"
-           (->> (detect-single-xor)
-                (map (comp (partial format "%02x") int))
-                (apply str)))))
+    (is (= "90cb2dc65138fac5a9a3ce1b5d570123"
+           (->> (detect-single-xor) .getBytes md5))))
 
   (testing "Challenge 5"
+    (let [input (str "Burning 'em, if you ain't quick and nimble\n"
+                     "I go crazy when I hear a cymbal")]
     (is (= (str "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"
                 "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")
-           (repeating-key-xor "ICE" (str "Burning 'em, if you ain't quick and nimble\n"
-                                         "I go crazy when I hear a cymbal")))))
+           (repeating-key-xor "ICE" input)))))
 
   (testing "Challenge 6"
     (is (= 37
-           (hamming-distance "this is a test" "wokka wokka!!!"))))
+           (hamming-distance "this is a test" "wokka wokka!!!")))
+
+    ;; Decypher the key:
+    (is (= "011327c9d6d57189d01c768cd3d6f4a3"
+           (->> (find-repeating-key-xor) byte-array md5)))
+
+    ;; Decypher the message:
+    (is (= "6187f6e338437e32f9cfc89ff6ee3b4d"
+           (->> (break-repeating-key-xor) .getBytes md5))))
 
   ;; end tests
   )
